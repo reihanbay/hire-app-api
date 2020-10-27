f1a052c5405f2c2a5850ef7421560df83359a19d
reihanbay <bung.reihan28@gmail.com>
2020-10-27 21:52:54 +0700

    add authAll inHire

--- src/routes/hire.js
+++ src/routes/hire.js
@@ -7,8 +7,8 @@ const { authRecruiter, authAll } = require('../middleware/auth')
 router.post('/', authRecruiter, createHire)
 router.get('/', authAll, getDataHire)
 router.get('/:id', authAll, getDataHireById)
-router.put('/:id', authRecruiter, updateHire)
-router.patch('/:id', authRecruiter, updatePatchHire)
+router.put('/:id', authAll, updateHire)
+router.patch('/:id', authAll, updatePatchHire)
 router.delete('/:id', authRecruiter, deleteHire)
 
 module.exports = router
